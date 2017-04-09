ffdd4791c122d71f8e8c12066b33ca4aeeb8a66e
Olaesean <mcbs@yandex.ru>
2017-04-09 20:31:42 +0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var opn = require('opn');
-var http = require('http');
-
-http.createServer(function (req, res) {
-res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
-
-opn('http://127.0.0.1:3000/');
